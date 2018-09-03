6e737932b037870994bcabf93a7ab936a3ba7076
Kasidis Arunruangsirilert <ken15@KenSoftTH-M5520.KenSoftTH.com>
2018-09-03 13:37:59 +0700

    Lecture8LabAssignment2

--- Lecture8/Lecture8LabAssignment2/Lecture8LabAssignment2.c
+++ Lecture8/Lecture8LabAssignment2/Lecture8LabAssignment2.c
@@ -1,14 +1,24 @@
 /* BG1221 Computer Programming For Engineering, 1/2018
 By Kasidis (Ken) Arunruangsirilert ID 6116803 Section 641
 Lab Assignment 8-2 Due 22th October 2018
-This program take 2 arrays input and find the sum of it*/
+This program written client account number, name, and balance to the file */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-int main() {
+void main() {
+	FILE *client; 
+	client = fopen("client.txt", "w");
+	fclose(client);
+	char name[30];
+	int acNumber;
+	double balance;
+	while (!feof(stdin)) {
+		client = fopen("client.txt", "a");
+		printf(": ");
+		scanf("%d %s %lf", &acNumber, &name, &balance);
+		fprintf(client, "%-6d %-30s %-10.2lf\n", acNumber, name, balance);
+		fclose(client);
+	}
+	
 
-
-
-
-	return 0;
 }
