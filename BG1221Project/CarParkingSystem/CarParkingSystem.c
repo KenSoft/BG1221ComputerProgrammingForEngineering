cd6b4379c4601f3a1b6ec6ff0aa0de6a9ef24ecb
Kasidis Arunruangsirilert <ken15@KenSoftTH-M5520.KenSoftTH.com>
2018-10-14 21:31:15 +0700

    Add Check if config exist

--- BG1221Project/CarParkingSystem/CarParkingSystem.c
+++ BG1221Project/CarParkingSystem/CarParkingSystem.c
@@ -2,3 +2,33 @@
 By Kasidis (Ken) Arunruangsirilert ID 6116803 Section 641
 Project Due 26th November 2018
 Just Another Car Parking Management System by KenSoftTH */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+void configIO();
+int checkConfigExist();
+
+int checkConfigExist() {
+	//Check if config.txt exists or not
+	FILE *file;
+	file = fopen("config.txt", "r");
+	if (file)
+	{
+		fclose(file);
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+void configIO() {
+	// This function read and write the config file
+	printf("%d", checkConfigExist());
+}
+
+
+void main() {
+	configIO();
+
+}
