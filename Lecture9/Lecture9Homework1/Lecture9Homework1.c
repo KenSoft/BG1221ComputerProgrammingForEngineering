9250e7959542c69f0f861747cd2dc5161149e0c1
Kasidis Arunruangsirilert <ken15@KenSoftTH-M5520.KenSoftTH.com>
2018-11-05 13:07:29 +0700

    Lecture 9 HW 1 done

--- Lecture9/Lecture9Homework1/Lecture9Homework1.c
+++ Lecture9/Lecture9Homework1/Lecture9Homework1.c
@@ -1,7 +1,7 @@
 /* BG1221 Computer Programming For Engineering, 1/2018
 By Kasidis (Ken) Arunruangsirilert ID 6116803 Section 641
 Homework 9-1 Due 12th November 2018
-This program take 2 integers and raised the power*/
+This program read the file and find maximum, minimum, and average of the dataset*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -14,21 +14,61 @@ void main() {
 	int max, min;
 	double avg;
 	FILE *file;
+	FILE *output;
 	file = fopen("input.txt", "r");
+	output = fopen("output.txt", "w");
+	printf("Dataset = ");
 	while (!feof(file))
 	{
+		if (count != 0) {
+			printf(", ");
+			fprintf(output ,", ");
+		}
 		fscanf(file, "%d", &inputArray[count]);
-		printf("%d\n", inputArray[count]);
+		printf("%d", inputArray[count]);
+		fprintf(output, "%d", inputArray[count]);
 		count++;
 	}
+	printf("\n");
+	fprintf(output, "\n");
 	max = maximum(inputArray);
+	min = minimum(inputArray);
+	avg = average(inputArray);
+
+	printf("Maximum = %d\nMinimum = %d\nAverage = %lf\n", max, min, avg);
+	fprintf(output, "Maximum = %d\nMinimum = %d\nAverage = %lf\n", max, min, avg);
 	fclose(file);
+	fclose(output);
 }
 
 int maximum(int inputArray[100]) {
 	int temp = inputArray[0], counter = 1;
 	for (counter; counter < count; counter++) {
-
+		if (temp < inputArray[counter]) {
+			temp = inputArray[counter];
+		}
 
 	}
+	return temp;
+}
+
+int minimum(int inputArray[100]) {
+	int temp = inputArray[0], counter = 1;
+	for (counter; counter < count; counter++) {
+		if (temp > inputArray[counter]) {
+			temp = inputArray[counter];
+		}
+
+	}
+	return temp;
+}
+
+double average(int inputArray[100]) {
+	int average = 0, counter = 0;
+	for (counter; counter < count; counter++) {
+		average = inputArray[counter] + average;
+	}
+	average = average / (double) count;
+	return average;
+
 }
